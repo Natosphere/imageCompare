04efe77ef87289c941d2e9ec009e71813edce300
Natosphere <nathanwheelwright@gmail.com>
2023-04-20 23:50:57 -0700

    restructure

--- files/compare.js
+++ files/compare.js
@@ -1,8 +1,18 @@
+// code to run when page is loaded
+document.addEventListener('DOMContentLoaded', function() {
+	updateImages
+	fitImage
+ }, false);
+
+
+
+
+
 // Get the DOM elements
 const imageContainer = document.getElementById('image-container');
 const image1 = document.getElementById('image1-display');
 const image2 = document.getElementById('image2-display');
-const fillSwitch = document.getElementById('fill-switch');
+const fitButton = document.getElementById('fit-button');
 const modeSwitch = document.getElementById('mode-switch');
 const input1 = document.getElementById('input1');
 const input2 = document.getElementById('input2');
@@ -12,43 +22,51 @@ let isImage1OnTop = true;
 
 // Function to update the images based on user input
 function updateImages() {
-	const url1 = URL.createObjectURL(input1.files[0]);
-	const url2 = URL.createObjectURL(input2.files[0]);
+	console.log("test");
+	var image1Name = image1.src.split("/").pop();
+	var image2Name = image2.src.split("/").pop();
 
-	if (url1 && url2) {
-		image1.src = url1;
-		image2.src = url2;
+	if (input1.files.length != 0 && input2.files.length != 0) {
+		var url1 = URL.createObjectURL(input1.files[0]);
+		var url2 = URL.createObjectURL(input2.files[0]);
+	} else {
+		var url1 = image1.src;
+		var url2 = image2.src;
+	}
 
-		if (fillSwitch.checked) {
-			if (image1.width / image1.height > image2.width / image2.height) {
-				image1.style.width = '100%';
-				image2.style.width = '100%';
-				image1.style.height = 'auto';
-				image2.style.height = 'auto';
-			} else {
-				image1.style.width = 'auto';
-				image2.style.width = 'auto';
-				image1.style.height = '100%';
-				image2.style.height = '100%';
-			}
-		} else {
-			image1.style.width = 'auto';
-			image2.style.width = 'auto';
-			image1.style.height = 'auto';
-			image2.style.height = 'auto';
-		}
+	image1.src = url1;
+	image2.src = url2;
 
-		if (modeSwitch.checked) {
-			image1.style.opacity = isImage1OnTop ? 1 : 0;
-			image2.style.opacity = isImage1OnTop ? 0 : 1;
-			imageLabel.innerText = isImage1OnTop ? 'Image 1 on top' : 'Image 2 on top';
-		} else {
-			image1.style.opacity = 1;
-			image2.style.opacity = 1;
-			imageLabel.innerText = '';
-		}
+	// toggle the image order
+	if (isImage1OnTop) {
+		isImage1OnTop = false;
+	} else {
+		isImage1OnTop = true;
+	}
+
+	if (modeSwitch.checked) {
+		image1.style.opacity = isImage1OnTop ? 1 : 0;
+		image2.style.opacity = isImage1OnTop ? 0 : 1;
+		imageLabel.innerText = isImage1OnTop ? image1Name : image2Name;
+	} else {
+		image1.style.opacity = 1;
+		image2.style.opacity = 1;
+		imageLabel.innerText = '';
 	}
 }
+
+
+// functon to fit the image to the screen
+function fitImage() {
+	if (image1.width > image1.height) {
+		imageContainer.style.width = '100%';
+		imageContainer.style.height = 'auto';
+	} else {
+		imageContainer.style.width = 'auto';
+		imageContainer.style.height = '100%';
+	}
+}
+
 
 
 // Function to handle file input changes
@@ -65,7 +83,7 @@ function handleFileInputChange(event) {
 }
 
 // Add event listeners to the switches
-fillSwitch.addEventListener('change', updateImages);
+fitButton.addEventListener('change', fitImage);
 modeSwitch.addEventListener('change', updateImages);
 
 // Add event listener to the image container for mode switch
@@ -79,3 +97,7 @@ imageContainer.addEventListener('click', () => {
 // Add event listeners to the file input fields
 input1.addEventListener('change', handleFileInputChange);
 input2.addEventListener('change', handleFileInputChange);
+
+// imageContainer.addEventListener('click', updateImages);
+image1.addEventListener('click', updateImages);
+image2.addEventListener('click', updateImages);
